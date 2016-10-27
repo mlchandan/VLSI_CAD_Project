67d4c924d2e61899dd68b232db07f8a1a557b812
mlchandan <ml.chandan7@gmail.com>
2016-10-28 01:59:31 +0530

    Added getBoolExpression()
    Made code compatible with Python3

--- final/BoolExprFunc.py
+++ final/BoolExprFunc.py
@@ -1,7 +1,11 @@
 from nodeclass import Node,Stack
 
-###########################################################################
 
+###########################################################################
+"""
+Convert infix boolean expression to postfix boolean expression
+
+"""
 def infixToPostfix(infixexpr):
 	opStack = Stack()
 	postfixList = []
@@ -28,8 +32,13 @@ def infixToPostfix(infixexpr):
 	
 	return " ".join(postfixList)
 
-###########################################################################
 
+
+###########################################################################
+"""
+Coverts postfix expression to Tree.
+
+"""
 def postfix_2_tree(postfix_list):
 	nodes_list=Stack()
 	fulllist=list()
@@ -49,12 +58,32 @@ def postfix_2_tree(postfix_list):
 			x= Node(token,left_node,right_node)
 			fulllist.append(x)
 			nodes_list.push(x)
+	fulllist.reverse() # to make root node come on top
 	for i in range(len(fulllist)):
-		fulllist[i].name= "n"+str(i)+"_"+fulllist[i].operator
-
+		fulllist[i].name= "n"+str(i)+"_"+fulllist[i].operator	
 	return nodes_list.pop(),fulllist
 
 
 
 
 ###########################################################################
+"""
+Given a parse tree/ subtree which would be representing the LUT,
+function returns the boolean expression for that subtree.
+
+"""
+def getBoolExpression(node, node_list):
+	node_list_name =[x.name for x in node_list]
+	if node.operator == "~":
+		return "~"+"( "+getBoolExpression(node.left,node_list)+" )"
+	if node.name in node_list_name:
+		if node.depth() == 0:
+			return node.name
+		else:
+			return "( "+getBoolExpression(node.left,node_list)+" "+node.operator +\
+			" "+getBoolExpression(node.right,node_list)+" )"
+	else:
+		return node.name
+
+###########################################################################
+
