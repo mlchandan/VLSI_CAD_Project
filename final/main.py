40248bdc404d84927f35ee08b8ca934ac1447907
mlchandan <ml.chandan7@gmail.com>
2016-10-30 00:19:43 +0530

    LUT mapping and logic comparison added

--- final/main.py
+++ final/main.py
@@ -2,12 +2,12 @@
 from nodeclass import *
 from BoolExprFunc import *
 from getlists import *
-
+import graphviz as gv
 
 ###########################################################################		
 
 #Initilisations
-Expression= "  ~ ( A  | ( B   &  C ) ) ^  ( D | E )  & ( F   &  G )  "
+Expression= "  ~ ( A  | ( B   &  C ) ) ^  ( D | E )  & ( F   &  E ) & ( V  &  W ) "
 LUT_input_size=6
 
 ###########################################################################		
@@ -30,11 +30,12 @@ print("\n POSTFIX Expression = ",postfix_list)
 
 Tree,tree_list = postfix_2_tree (postfix_list)
 
+
 print("\nNumber of  Nodes in the tree obtained from Boolean expression = ",str(len(tree_list)))
 
 
 print("\nThe Binary nodes of the tree are :\n")
-print("Node left_child Right_child")
+print("Node left_child Right_child depth")
 for i in tree_list:
 	print(i.name , " ", end=" ")
 	if i.left != None:
@@ -42,9 +43,13 @@ for i in tree_list:
 	else:
 		print(None, end=" ")
 	if i.right != None:
-		print(i.right.name ," ")
+		print(i.right.name)
 	else:
 		print(None)
+	
+###########################################################################
+
+
 	
 
 ###########################################################################
@@ -87,29 +92,35 @@ print_lists_of_lists_of_nodes(set1)
 #Stage 3 progress :
 #Get boolean expressions of the LUT splits.
 #Define LUT class and fill the memory array using Pyeda
-
 """
 
+boolExp=[]
+lut_list=[]
 print("\n\nThe boolean expression for each parse tree =")
 for parse_tree in set1:
 	parse_tree.sort(key=lambda x: x.depth(), reverse=True)
 	print("parse tree=", end=" ")
 	print_list_of_nodes(parse_tree)
-	print("Expression=", getBoolExpression(parse_tree[0],parse_tree))
-	print()
-
-
-
-
+	print("Expression:",parse_tree[0].name +"="+ getBoolExpression(parse_tree[0],parse_tree))
+	eq =getBoolExpression(parse_tree[0],parse_tree)
+	boolExp.append(eq)
+	lut_list.append(LUT(eq,parse_tree[0],LUT_input_size))
+	print('\n')
 
 ###########################################################################
 
 """
-#Stage 4 :
+#stage 3 : Completed
+#Stage 4 : In progress
 Connect the LUTS to act as Event driven logic.
 Make test.py to test the LUT logic with boolean expression logic.
 Represent the boolean tree in GUI form.
 """
+#intial_inputs={'A':0,'B':0,'C':0,'D':0,'E':0,'F':0}
+#print(lut_list[0].getOutput(intial_inputs,lut_list))
+
+
+VerifyLUTset(Expression, lut_list , tree_list )
 
 ###########################################################################
 
