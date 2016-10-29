ebd9635c3088654ec3fc5eacdd04a80438e68f31
mlchandan <ml.chandan7@gmail.com>
2016-10-30 00:17:27 +0530

    LUT logic verification Added
    Using PyEDA truthtable to compare the logic

--- final/BoolExprFunc.py
+++ final/BoolExprFunc.py
@@ -1,4 +1,5 @@
-from nodeclass import Node,Stack
+from nodeclass import Node,Stack,LUT
+from pyeda.inter import *
 
 
 ###########################################################################
@@ -42,29 +43,41 @@ Coverts postfix expression to Tree.
 def postfix_2_tree(postfix_list):
 	nodes_list=Stack()
 	fulllist=list()
+	input_list=[]
 	for token in postfix_list.split():
 		if token in "ABCDEFGHIJKLMNOPQRSTUVWXYZ" or token in "0123456789":
 			x= Node(token)
-			fulllist.append(x)
+			if token not in [each.operator for each in input_list]:
+				input_list.append(x)  ###
+			#fulllist.append(x)
 			nodes_list.push(x)
-		elif token == '~':
+
+		elif token in '~!':
 			left_node = nodes_list.pop()
+			if left_node.operator in [each.operator for each in input_list]:
+				left_node=next((x for x in input_list if x.operator == left_node.operator), None)
 			x= Node(token,left_node)
 			fulllist.append(x)
 			nodes_list.push(x)
+
 		elif token in '&|^':
 			left_node = nodes_list.pop()
+
+			if left_node.operator in [each.operator for each in input_list]:
+				left_node = next((x for x in input_list if x.operator == left_node.operator), None)
 			right_node = nodes_list.pop()
+			if right_node.operator in [each.operator for each in input_list]:
+				right_node=next((x for x in input_list if x.operator == right_node.operator), None)
+				
 			x= Node(token,left_node,right_node)
 			fulllist.append(x)
-			nodes_list.push(x)
+			nodes_list.push(x)			
 	fulllist.reverse() # to make root node come on top
 	for i in range(len(fulllist)):
 		fulllist[i].name= "n"+str(i)+"_"+fulllist[i].operator	
-	return nodes_list.pop(),fulllist
-
-
-
+	for i in input_list:
+			i.name=i.operator
+	return nodes_list.pop(),fulllist+input_list
 
 ###########################################################################
 """
@@ -75,7 +88,7 @@ function returns the boolean expression for that subtree.
 def getBoolExpression(node, node_list):
 	node_list_name =[x.name for x in node_list]
 	if node.operator == "~":
-		return "~"+"( "+getBoolExpression(node.left,node_list)+" )"
+		return "~"+" ( "+getBoolExpression(node.left,node_list)+" )"
 	if node.name in node_list_name:
 		if node.depth() == 0:
 			return node.name
@@ -87,3 +100,34 @@ def getBoolExpression(node, node_list):
 
 ###########################################################################
 
+def VerifyLUTset(Expression, lut_list , tree_list ):
+	#Actual Boolean expression
+	pyeda_exp =expr(Expression)
+	print(str(pyeda_exp))
+	a = expr2truthtable(pyeda_exp)
+
+	#Derived Boolean expression and truth table
+	intial_inputs={}
+	listalp=sorted([x.name for x in tree_list if x.depth()==0])
+	for i in range(len(listalp)):
+		intial_inputs[listalp[i]]=0
+	out=''
+
+	for i in range(2**len(listalp)):
+		in1 =bin(i)[2:].zfill(len(listalp)) 
+		in1=in1[::-1]
+		for l in range(len(listalp)):
+			intial_inputs[listalp[l]]= int(in1[l])
+		out+= str( lut_list[0].getOutput(intial_inputs,lut_list) )
+
+	ab ="".join(listalp)
+	f = truthtable( map(exprvar,ab),out)
+
+
+	print('\n\n')
+	#print derived truth table
+	print('TRUTH TABLE OF THE LUT :')
+	print(f )
+
+	#Compare TRuth Table
+	print("Result of Logic Verification : " , str(f) == str(a))
